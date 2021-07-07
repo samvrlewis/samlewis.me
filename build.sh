0f181fff3a508540ffd34b3defa190a91cc397fa
Sam Lewis <sam.vr.lewis@gmail.com>
2021-07-07 20:31:02 +1000

    Attempt to build with actions

--- build.sh
+++ build.sh
@@ -3,13 +3,15 @@
 #report to console
 set -e
 
+sudo apt-get -y install pelican
+
 #make the website
 pelican  content -o output -s pelicanconf.py
 
 #clean old master dir
 rm -rf master
 
-git clone https://${GH_TOKEN}@github.com/samvrlewis/samvrlewis.github.io.git master
+git clone https://github.com/samvrlewis/samvrlewis.github.io.git master
 
 #copy to master
 cp -R output/* master
@@ -18,7 +20,7 @@ cd master
 git config user.email "sam.vr.lewis@gmail.com"
 git config user.name "Sam Lewis"
 git add -A .
-git commit -a -m "Travis #$TRAVIS_BUILD_NUMBER"
+git commit -a -m "Actions #$GITHUB_RUN_ID"
 
 #hide the output so token isn't leaked
 git push origin master
